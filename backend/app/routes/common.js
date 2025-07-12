ec3048c95971ad85e1dc6f0957896a918829639d
ismaeilalrewany <ismailalrewany332@gmail.com>
2025-07-13 02:06:46 +0300

    refactor(routes/common): Change Controller to CommonsController

--- backend/app/routes/common.js
+++ backend/app/routes/common.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import auth from '../middleware/auth.js'
-import Controller from '../controllers/common.js'
+import CommonsController from '../controllers/common.js'
 
 const router = express.Router()
 
-router.get('/check-auth', auth, Controller.checkAuth)
-router.get('/check-server', Controller.checkServer)
+router.get('/check-auth', auth, CommonsController.checkAuth)
+router.get('/check-server', CommonsController.checkServer)
 
 export default router
