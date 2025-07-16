3c2e12bf2f7b61d8218f8fa209728269605933ef
ismaeilalrewany <ismailalrewany332@gmail.com>
2025-07-16 05:15:38 +0300

    refactor(NotFoundPage): Better UI for NotFoundPage.jsx

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -2,16 +2,20 @@ import { Link } from 'react-router-dom'
 
 const NotFoundPage = () => {
   return (
-    <main className="flex-grow flex items-center justify-center container mx-auto p-4">
-      <div className="card w-full max-w-lg bg-white shadow-xl text-center">
-        <div className="card-body p-10">
-          <h1 className="text-8xl font-bold text-black">404</h1>
-          <h2 className="card-title text-3xl font-semibold mt-4 text-gray-800">Page Not Found</h2>
-          <p className="py-4 text-gray-500">Oops! The page you are looking for does not exist. It might have been moved or deleted.</p>
-          <div className="card-actions justify-center mt-4">
-            <Link to="/" className="btn bg-black text-white normal-case text-base font-medium px-8 hover:bg-gray-800" >Go Back Home</Link>
-          </div>
-        </div>
+    <main className="bg-gradient-to-br from-base-200 to-base-100">
+      <div className="container mx-auto px-2">
+        <section className="flex justify-center items-center h-screen">
+          <article className="card w-full max-w-lg bg-white shadow-lg text-center">
+            <div className="card-body p-10 flex flex-col items-center">
+              <h1 className="text-8xl font-semibold text-neutral">404</h1>
+              <h2 className="card-title text-3xl font-semibold mt-4 text-neutral/80">Page Not Found</h2>
+              <p className="py-4 text-neutral/60">Oops! The page you are looking for does not exist. It might have been moved or deleted.</p>
+              <div className="card-actions justify-center mt-4">
+                <Link to="/" className="btn bg-neutral text-neutral-content normal-case text-base font-medium px-8 border-0" >Go Back Home</Link>
+              </div>
+            </div>
+          </article>
+        </section>
       </div>
     </main>
   )
